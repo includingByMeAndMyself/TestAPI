f9b11fead28f0b7a0a5a06069da2ae3f193c3409
includingivansmirnov@gmail.com <includingivansmirnov@gmail.com>
2022-02-13 19:11:23 +0500

    Add project files.

---
+++ TestAPI/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Timesheet.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet]
+        public string Get(string name)
+        {
+            return name;
+        }
+    }
+}
